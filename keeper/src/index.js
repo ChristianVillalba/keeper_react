27104602cf8441222ef6d4ea3fe651db70b593f0
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-07-16 13:41:47 +0100

    components created: Header.jsx & Footer.jsx and successully connected to App.jsx

--- keeper/src/index.js
+++ keeper/src/index.js
@@ -11,6 +11,13 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
