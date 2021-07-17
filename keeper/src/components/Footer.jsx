dc6a4fe9bfc7a08c4b9833380e7a1e92e0bff7b7
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-07-17 18:24:43 +0100

    adds styles

--- keeper/src/components/Footer.jsx
+++ keeper/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const currentYear = new Date().getFullYear();
 function Footer() {
-  return <p>Copyright: {currentYear}</p>;
+  return <p className="footer">Copyright: {currentYear}</p>;
 }
 
 export default Footer;
--- keeper/src/components/Note.jsx
+++ keeper/src/components/Note.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Note() {
-  return <div>
+  return <div className="note">
   <h1>Note</h1>
   <p>Paragraph</p>
   </div>;
